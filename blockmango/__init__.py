79fc4eaf8bcadc0a94e17207493a9548279e75da
Matt300793 <mattmattbg2100@gmail.com>
2026-05-13 20:01:22 +0800

    Add imports for Clan, Group, Friends, and Decoration
    Import additional modules for Clan, Group, Friends, and Decoration.

--- blockmango/__init__.py
+++ blockmango/__init__.py
@@ -1,5 +1,8 @@
-# blockmango/__init__.py
-from .user import User
-from .http import HTTPMixin
-# Only add the others (Clan, Friends) once the server boots successfully
+# Inside blockmango/__init__.py
 
+from .clan import Clan
+from .user import User
+from .groupchat import Group
+from .friends import Friends
+from .decoration import Decoration
+from .http import HTTPMixin
