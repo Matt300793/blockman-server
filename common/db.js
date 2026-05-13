6c1208341a634c6dc0774b484c35820f4f1c498f
Matt300793 <mattmattbg2100@gmail.com>
2026-05-13 20:36:48 +0800

    Delete common directory

--- common/db.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const SQLiteDB = require('./sqlite'); // Your SQLiteDB class file (the big class you posted)
-
-// Create and export a single shared instance:
-const dbFile = path.resolve(__dirname, 'database.db');
-const db = new SQLiteDB(dbFile);
-
-module.exports = db;
